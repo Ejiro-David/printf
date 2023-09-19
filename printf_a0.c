8f0b4af08177eb975dacc386ef74ece11a0f3e53
“Ejiro-David” <daviderhabor.de@gmail.com>
2023-09-19 10:17:29 -0700

    more reds

--- printf_a0.c
+++ printf_a0.c
@@ -1,38 +1,39 @@
 #include "main.h"
+#include <unistd.h>
 
 int _printf(const char *format, ...)
 {
 	int chara_print = 0;
 	va_list list_of_args;
 
-	if (format == NULL);
+	if (format == NULL)
+	{
 		return -1;
+	}
 
 	va_start(list_of_args, format);
 
 	while (*format)  
 	{
-		if (*format != "%") // if format is not % sign
+		if (*format != '%')
 		{
-			write(1, format, 1); Write the char to the standard output
+			write(1, format, 1); 
 			chara_print++;
 		}
-		else // If format is a % sign
+		else
 		{
-			format++; // Bypass "%" - Check the next character
+			format++; 
 			if (*format == '\0')
 				break;
 
-			if (*format == '%') // This solves %%
+			if (*format == '%') 
 			{
-				// Manage '%%' (double '%')
 				write(1, format, 1);
 				chara_print++;
 			}
 			else if (*format == 'c')
 			{
-				// Manage '%c' (character)
-				char c = va_arg(list_of_args, int); //
+				char c = va_arg(list_of_args, int);
 				write(1, &c, 1);
 				chara_print++;
 			}
@@ -41,11 +42,9 @@ int _printf(const char *format, ...)
 				char *str = va_arg(list_of_args, char*);
 				int str_len = 0;
 
-				// Calculate the length of a string
 				while (str[str_len] != '\0')
 					str_len++;
 				
-				// Write the strinf to the standard output
 				write(1, str, str_len);
 				chara_print += str_len;
 			}
@@ -55,5 +54,7 @@ int _printf(const char *format, ...)
 	va_end(list_of_args);
 
 	return chara_print;
+
+
 }
 
