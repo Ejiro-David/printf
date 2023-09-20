11c5f13eb0cdfd1d104ad9e096e4b7f87cfe574f
PahyP <pash.shivuri@gmail.com>
2023-09-19 20:15:52 -0700

    few warnings

--- printf_a0.c
+++ printf_a0.c
@@ -10,41 +10,44 @@
  */
 
 int _itoa(char *str, int num);
-int _itoa(char *str, int num){
+int _itoa(char *str, int num)
+{
 	int i = 0, start, end, temp;
 
-    if (num == 0)
-    {
+	if (num == 0)
+	{
 	    str[i++] = '0';
-    }
-    else
-    {
-	    if (num < 0)
-	    {
+	}
+	else
+	{
+		if (num < 0)
+		{
 		    str[i++] = '-';
 		    num = -num;
-	    }
-	    temp = num;
-	    while(temp != 0)
-	    {
+		}
+		temp = num;
+		while (temp != 0)
+		{
 		    int digit = temp % 10;
+
 		    str[i++] = digit + '0';
 		    temp /= 10;
-	   }
-	    start = (str[0] == '-') ? 1 : 0;
-	    end = i - 1;
+		}
+		start = (str[0] == '-') ? 1 : 0;
+		end = i - 1;
 
-	    while (start < end)
-	    {
+		while (start < end)
+		{
 		    char temp = str[start];
+
 		    str[start] = str[end];
 		    str[end] = temp;
 		    start++;
 		    end--;
-	    }
-    }
-    str[i] = '\0';
-    return i;
+		}
+	}
+	str[i] = '\0';
+	return (i);
 }
 
 /**
