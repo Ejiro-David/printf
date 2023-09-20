7146d4a3ce4adf8b7e78962a2cb2f45be17970ed
“Ejiro-David” <daviderhabor.de@gmail.com>
2023-09-19 19:24:53 -0700

    updated

--- printf_a0.c
+++ printf_a0.c
@@ -2,41 +2,56 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int _itoa(char *str, int num) {
+/**
+ * _itoa - converts int to string
+ * @str: pointer to string converted
+ * @num: number to be converted
+ * Return: lenght ot number
+ */
+
+int _itoa(char *str, int num);
+int _itoa(char *str, int num){
 	int i = 0, start, end, temp;
 
-    if (num == 0) {
-        str[i++] = '0';
-    } else {
-        if (num < 0) {
-            str[i++] = '-';
-            num = -num;
-        }
-
-        temp = num;
-        while (temp != 0) {
-            int digit = temp % 10;
-            str[i++] = digit + '0';
-            temp /= 10;
-        }
-
-        start = (str[0] == '-') ? 1 : 0;
-        end = i - 1;
-
-        while (start < end) {
-            char temp = str[start];
-            str[start] = str[end];
-            str[end] = temp;
-            start++;
-            end--;
-        }
+    if (num == 0)
+    {
+	    str[i++] = '0';
     }
+    else
+    {
+	    if (num < 0)
+	    {
+		    str[i++] = '-';
+		    num = -num;
+	    }
+	    temp = num;
+	    while(temp != 0)
+	    {
+		    int digit = temp % 10;
+		    str[i++] = digit + '0';
+		    temp /= 10;
+	   }
+	    start = (str[0] == '-') ? 1 : 0;
+	    end = i - 1;
 
+	    while (start < end)
+	    {
+		    char temp = str[start];
+		    str[start] = str[end];
+		    str[end] = temp;
+		    start++;
+		    end--;
+	    }
+    }
     str[i] = '\0';
     return i;
 }
 
-
+/**
+ * _printf - prints formated string to standard output
+ * @format: format to follow.
+ * Return: lenght of output string
+ */
 
 int _printf(const char *format, ...);
 int _printf(const char *format, ...)
