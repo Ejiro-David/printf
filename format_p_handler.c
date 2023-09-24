2d89a2056a57966fedc9f8d93e6f8b3cf1f78ad3
PashyP <pash.shivuri@gmail.com>
2023-09-24 07:46:24 -0700

    close

--- format_p_handler.c
+++ format_p_handler.c
@@ -2,17 +2,17 @@
 
 int format_p(char *buffer, int buffer_index, va_list list_of_args)
 {
-    int len;
-    void *ptr = va_arg(list_of_args, void *);
-    unsigned long address = (unsigned long)ptr;
-    char hex_address[18];
-    sprintf(hex_address, "0x%lx", address);
-    len = strlen(hex_address);
+	int len;
+	void *ptr = va_arg(list_of_args, void *);
+	unsigned long address = (unsigned long)ptr;
+	char hex_address[18];
 
-    for (int i = 0; i < len; i++)
-    {
-        buffer[buffer_index++] = hex_address[i];
-    }
-    return buffer_index;
+	sprintf(hex_address, "0x%lx", address);
+	len = strlen(hex_address);
+
+	for (int i = 0; i < len; i++)
+	{
+		buffer[buffer_index++] = hex_address[i];
+	}
+	return (buffer_index);
 }
-
